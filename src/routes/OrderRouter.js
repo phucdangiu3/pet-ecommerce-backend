2bc47e5bae902a728380996b761c219b101b65df
Phuc dep zai <phanhoangphuc2906@gmail.com>
2025-06-08 16:52:38 +0700

    Fix authentication and update order route

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const OrderController = require("../controllers/OrderController");
 const { authUserMiddleWare } = require("../middleware/authMiddleware");
 
-router.post("/create", authUserMiddleWare, OrderController.createOrder);
+router.post("/create", OrderController.createOrder);
 router.get("/get-all-order/:id", OrderController.getAllOrderDetails);
 router.get("/get-details-order/:id", OrderController.getDetailsOrder);
 router.delete("/cancel-order/:id", OrderController.CancelOrderDetails);
